80cc9c6290d8323a6b44cd24c2104ddc2a04dd5c
Moshe Immerman <moshe@flanksource.com>
2022-04-08 14:32:54 +0300

    chore(ui): bump ui and switch to full app

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,7 +1,8 @@
+import { App } from "@flanksource/flanksource-ui/dist/";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+
 
 ReactDOM.render(
   <React.StrictMode>
