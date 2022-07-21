43c70ae28c4e9d7211ce1e51395c260f56efe1b7
Moshe Immerman <moshe@flanksource.com>
2022-07-21 22:06:54 +0200

    chore: remove pre-emptive seed.sql

--- db/migrations/6_seed.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-
-
--- Update the version_id to the latest version, so that the seed scripts can always reflect the desired state, and the versions post 6
--- are only used to migrate old databases, not setup new ones.
-INSERT INTO incident_commander_db_version (version_id,is_applied,tstamp) values ('6',true, now());
-
--- +goose StatementEnd
