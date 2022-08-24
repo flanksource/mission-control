cf331424aa98a5638cee65077440c378862452ac
Yash Mehrotra <yashmehrotra95@gmail.com>
2022-08-24 11:41:38 +0530

    lint: remove unused function from responder

--- responder/events.go
+++ responder/events.go
@@ -124,30 +124,6 @@ func NotifyJiraResponderAddComment(responder api.Responder, comment string) (str
 	return commentObj.ID, nil
 }
 
-func msPlannerClientFromResponder(responder api.Responder) (msplanner.MSPlannerClient, error) {
-	if responder.Properties["responderType"] != MSPlannerResponder {
-		return msplanner.MSPlannerClient{}, fmt.Errorf("invalid responderType: %s", responder.Properties["responderType"])
-	}
-
-	teamSpecJson, err := responder.Team.Spec.MarshalJSON()
-	if err != nil {
-		return msplanner.MSPlannerClient{}, err
-	}
-
-	var teamSpec api.TeamSpec
-	if err = json.Unmarshal(teamSpecJson, &teamSpec); err != nil {
-		return msplanner.MSPlannerClient{}, err
-	}
-
-	return msplanner.NewClient(
-		teamSpec.ResponderClients.MSPlanner.TenantID,
-		teamSpec.ResponderClients.MSPlanner.ClientID,
-		teamSpec.ResponderClients.MSPlanner.GroupID,
-		teamSpec.ResponderClients.MSPlanner.Username.Value,
-		teamSpec.ResponderClients.MSPlanner.Password.Value,
-	)
-}
-
 func NotifyMSPlannerResponderAddComment(responder api.Responder, comment string) (string, error) {
 	if responder.Properties["responderType"] != MSPlannerResponder {
 		return "", fmt.Errorf("invalid responderType: %s", responder.Properties["responderType"])
