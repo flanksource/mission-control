bd8db9709ea182b72bcd8c1628d0efa2d72f0f48
Yash Mehrotra <yashmehrotra95@gmail.com>
2022-08-26 11:24:33 +0530

    feat: sync comments from jira to incident commander

---
+++ responder/comments_sync.go
@@ -0,0 +1,76 @@
+package responder
+
+import (
+	"database/sql"
+	"time"
+
+	"github.com/flanksource/commons/collections"
+	"github.com/flanksource/commons/logger"
+
+	"github.com/flanksource/incident-commander/api"
+	"github.com/flanksource/incident-commander/db"
+)
+
+func StartCommentSync() {
+	for {
+		logger.Infof("Syncing comments")
+		syncComments()
+
+		time.Sleep(1 * time.Hour)
+	}
+}
+
+func syncComments() {
+	var responders []api.Responder
+	err := db.Gorm.Where("external_id IS NOT NULL").Preload("Team").Find(&responders).Error
+	if err != nil {
+		logger.Errorf("Error fetching responders from database: %v", err)
+		return
+	}
+
+	dbSelectExternalIDQuery := `
+        SELECT external_id FROM comments WHERE responder_id = @responder_id
+        UNION
+        SELECT external_id FROM comment_responders WHERE responder_id = @responder_id
+    `
+
+	for _, responder := range responders {
+		teamSpec, err := responder.Team.GetSpec()
+		if err != nil {
+			logger.Errorf("Error getting team spec: %v", err)
+			continue
+		}
+
+		if responder.Properties["type"] == JiraResponder {
+			jiraClient, err := jiraClientFromTeamSpec(teamSpec)
+			if err != nil {
+				logger.Errorf("Error instantiating Jira client: %v", err)
+				continue
+			}
+
+			responderComments, err := jiraClient.GetComments(responder.ExternalID)
+			if err != nil {
+				logger.Errorf("Error fetching comments from Jira: %v", err)
+				continue
+			}
+
+			// Query all external_ids from comments and comment_responders table
+			var dbExternalIDs []string
+			err = db.Gorm.Raw(dbSelectExternalIDQuery, sql.Named("responder_id", responder.ID)).Find(&dbExternalIDs).Error
+			if err != nil {
+				logger.Errorf("TODO")
+				continue
+			}
+
+			// IDs which are in Jira but not added to database must be added in the comments table
+			for _, responderComment := range responderComments {
+				if !collections.Contains(dbExternalIDs, responderComment.ExternalID) {
+					err = db.Gorm.Create(&responderComment).Error
+					if err != nil {
+						logger.Errorf("Error inserting comment in database: %v", err)
+					}
+				}
+			}
+		}
+	}
+}
