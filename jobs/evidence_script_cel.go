82be540e9570c2ba709d8bd7be33a59dd3f29b66
Yash Mehrotra <yashmehrotra95@gmail.com>
2022-12-12 22:33:49 +0530

    chore: use cache for storing cel programs

--- jobs/evidence_script_cel.go
+++ jobs/evidence_script_cel.go
@@ -3,16 +3,20 @@ package jobs
 import (
 	"fmt"
 	"strconv"
+	"time"
 
-	//"github.com/flanksource/incident-commander/api"
 	"github.com/flanksource/incident-commander/db"
 
 	"github.com/flanksource/commons/logger"
 	"github.com/google/cel-go/cel"
+	"github.com/patrickmn/go-cache"
 )
+
+var prgCache *cache.Cache
 
 func EvaluateEvidenceScripts() {
 	logger.Debugf("Evaluating evidence scripts")
+	prgCache = cache.New(24*time.Hour, 1*time.Hour)
 
 	// Fetch all evidences of open incidents which have a script
 	evidences := db.GetEvidenceScripts()
@@ -38,26 +42,11 @@ func EvaluateEvidenceScripts() {
 }
 
 func evaluate(evidence db.EvidenceScriptInput) (string, error) {
-	env, err := cel.NewEnv(
-		cel.Variable("config", cel.AnyType),
-		cel.Variable("component", cel.AnyType),
-		cel.Variable("incident", cel.AnyType),
-	)
+	prg, err := getCELPrgFromCache(evidence)
 	if err != nil {
 		return "", err
 	}
-
-	ast, iss := env.Compile(evidence.Script)
-	if iss.Err() != nil {
-		return "", iss.Err()
-	}
-
-	prg, err := env.Program(ast)
-	if err != nil {
-		return "", err
-	}
-
-	out, _, err := prg.Eval(map[string]any{
+	out, _, err := (*prg).Eval(map[string]any{
 		"config":    evidence.ConfigItem.Config,
 		"component": evidence.Component.AsMap(),
 		"incident":  evidence.Hypothesis.Incident,
@@ -67,4 +56,30 @@ func evaluate(evidence db.EvidenceScriptInput) (string, error) {
 	}
 
 	return fmt.Sprint(out), nil
+}
+
+func getCELPrgFromCache(evidence db.EvidenceScriptInput) (*cel.Program, error) {
+	if prg, exists := prgCache.Get(evidence.Script); exists {
+		return prg.(*cel.Program), nil
+	}
+	env, err := cel.NewEnv(
+		cel.Variable("config", cel.AnyType),
+		cel.Variable("component", cel.AnyType),
+		cel.Variable("incident", cel.AnyType),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	ast, iss := env.Compile(evidence.Script)
+	if iss.Err() != nil {
+		return nil, iss.Err()
+	}
+
+	prg, err := env.Program(ast)
+	if err != nil {
+		return nil, err
+	}
+	prgCache.SetDefault(evidence.Script, &prg)
+	return &prg, nil
 }
