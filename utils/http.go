1d1b1c8e88eb9ba62be65df092c158cc16caeb49
Yash Mehrotra <yashmehrotra95@gmail.com>
2022-12-20 18:38:38 +0530

    chore: implement httpget function

---
+++ utils/http.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"io/ioutil"
+	"net/http"
+)
+
+func HTTPGet(url string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
+}
