f0dfc018f3ab6aaa5d17f3820c6576aa1129ac4e
Yash Mehrotra <yashmehrotra95@gmail.com>
2023-01-17 12:55:33 +0530

    feat: cleanup job history table

--- db/job_history.go
+++ db/job_history.go
@@ -17,3 +17,17 @@ func PersistJobHistory(h *models.JobHistory) error {
 
 	return Gorm.Table("job_history").Save(h).Error
 }
+
+func DeleteOldJobHistoryRows(keepLatest int) error {
+	return Gorm.Exec(`
+    WITH ordered_history AS (
+        SELECT
+            id, resource_type, resource_id, name, created_at,
+            ROW_NUMBER() OVER (PARTITION by resource_id, resource_type, name ORDER BY created_at DESC)
+        FROM job_history
+    )
+    DELETE FROM job_history WHERE id IN (
+        SELECT id FROM ordered_history WHERE row_number > ?
+    )
+    `, keepLatest).Error
+}
