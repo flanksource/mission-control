b018829063dab51023086bda15cb5986edc5de03
Aditya <thebeaditya@gmail.com>
2023-02-21 11:32:34 +0545

    add support for all tables in upstream controller

--- events/upstream_push.go
+++ events/upstream_push.go
@@ -27,7 +27,7 @@ func newPushToUpstreamEventHandler(conf api.UpstreamConfig) *pushToUpstreamEvent
 	}
 }
 
-// pushToUpstream pushes data from decentralized instances to central incident commander
+// Run pushes data from decentralized instances to central incident commander
 func (t *pushToUpstreamEventHandler) Run(ctx context.Context, tx *gorm.DB, event api.Event) error {
 	pushQueueID := event.Properties["id"]
 	var changelogs []models.PushQueue
@@ -48,62 +48,62 @@ func (t *pushToUpstreamEventHandler) Run(ctx context.Context, tx *gorm.DB, event
 		switch tablename {
 		case "components":
 			if err := tx.Where("id IN ?", itemIDs).Find(&upstreamMsg.Components).Error; err != nil {
-				return fmt.Errorf("error fetching components; %w", err)
+				return fmt.Errorf("error fetching components: %w", err)
 			}
 
 		case "canaries":
 			if err := tx.Where("id IN ?", itemIDs).Find(&upstreamMsg.Canaries).Error; err != nil {
-				return fmt.Errorf("error fetching canaries; %w", err)
+				return fmt.Errorf("error fetching canaries: %w", err)
 			}
 
 		case "checks":
 			if err := tx.Where("id IN ?", itemIDs).Find(&upstreamMsg.Checks).Error; err != nil {
-				return fmt.Errorf("error fetching checks; %w", err)
+				return fmt.Errorf("error fetching checks: %w", err)
 			}
 
 		case "config_analysis":
 			if err := tx.Where("id IN ?", itemIDs).Find(&upstreamMsg.ConfigAnalysis).Error; err != nil {
-				return fmt.Errorf("error fetching config_analysis; %w", err)
+				return fmt.Errorf("error fetching config_analysis: %w", err)
 			}
 
 		case "config_changes":
 			if err := tx.Where("id IN ?", itemIDs).Find(&upstreamMsg.ConfigChanges).Error; err != nil {
-				return fmt.Errorf("error fetching config_changes; %w", err)
+				return fmt.Errorf("error fetching config_changes: %w", err)
 			}
 
 		case "config_items":
 			if err := tx.Where("id IN ?", itemIDs).Find(&upstreamMsg.ConfigItems).Error; err != nil {
-				return fmt.Errorf("error fetching config_items; %w", err)
+				return fmt.Errorf("error fetching config_items: %w", err)
 			}
 
 		case "check_statuses":
 			compositeKeys := splitKeys(itemIDs)
 			if err := tx.Where(`(check_id, "time") IN ?`, compositeKeys).Find(&upstreamMsg.CheckStatuses).Error; err != nil {
-				return fmt.Errorf("error fetching check_statuses; %w", err)
+				return fmt.Errorf("error fetching check_statuses: %w", err)
 			}
 
 		case "config_component_relationships":
 			compositeKeys := splitKeys(itemIDs)
 			if err := tx.Where("(component_id, config_id) IN ?", compositeKeys).Find(&upstreamMsg.ConfigItems).Error; err != nil {
-				return fmt.Errorf("error fetching config_component_relationships; %w", err)
+				return fmt.Errorf("error fetching config_component_relationships: %w", err)
 			}
 
 		case "component_relationships":
 			compositeKeys := splitKeys(itemIDs)
 			if err := tx.Where("(component_id, relationship_id, selector_id) IN ?", compositeKeys).Find(&upstreamMsg.ComponentRelationships).Error; err != nil {
-				return fmt.Errorf("error fetching component_relationships; %w", err)
+				return fmt.Errorf("error fetching component_relationships: %w", err)
 			}
 
 		case "config_relationships":
 			keys := splitKeys(itemIDs)
 			if err := tx.Where("(related_id, config_id, selector_id) IN ?", keys).Find(&upstreamMsg.ConfigRelationships).Error; err != nil {
-				return fmt.Errorf("error fetching config_relationships; %w", err)
+				return fmt.Errorf("error fetching config_relationships: %w", err)
 			}
 		}
 	}
 
 	if err := t.push(ctx, upstreamMsg); err != nil {
-		return fmt.Errorf("failed to push to upstream; %w", err)
+		return fmt.Errorf("failed to push to upstream: %w", err)
 	}
 
 	return nil
@@ -112,23 +112,23 @@ func (t *pushToUpstreamEventHandler) Run(ctx context.Context, tx *gorm.DB, event
 func (t *pushToUpstreamEventHandler) push(ctx context.Context, msg *api.PushData) error {
 	payloadBuf := new(bytes.Buffer)
 	if err := json.NewEncoder(payloadBuf).Encode(msg); err != nil {
-		return fmt.Errorf("error encoding msg; %w", err)
+		return fmt.Errorf("error encoding msg: %w", err)
 	}
 
 	req, err := http.NewRequest(http.MethodPost, t.conf.URL, payloadBuf)
 	if err != nil {
-		return fmt.Errorf("http.NewRequest; %w", err)
+		return fmt.Errorf("http.NewRequest: %w", err)
 	}
 
 	req.SetBasicAuth(t.conf.Username, t.conf.Password)
 	resp, err := t.httpClient.Do(req)
 	if err != nil {
-		return fmt.Errorf("error making request; %w", err)
+		return fmt.Errorf("error making request: %w", err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusCreated && resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("upstream server returned error status; %s", resp.Status)
+		return fmt.Errorf("upstream server returned error status: %s", resp.Status)
 	}
 
 	return nil
--- upstream/controllers.go
+++ upstream/controllers.go
@@ -35,9 +35,22 @@ func UpstreamPushesCtrl(c echo.Context) error {
 }
 
 func insertUpstreamMsg(ctx context.Context, req *api.PushData) error {
+	if len(req.Canaries) > 0 {
+		if err := db.Gorm.Clauses(clause.OnConflict{UpdateAll: true}).Create(req.Canaries).Error; err != nil {
+			return fmt.Errorf("error upserting canaries; %w", err)
+		}
+	}
+
 	if len(req.Components) > 0 {
 		if err := db.Gorm.Clauses(clause.OnConflict{UpdateAll: true}).Create(req.Components).Error; err != nil {
 			return fmt.Errorf("error upserting components; %w", err)
+		}
+	}
+
+	if len(req.ComponentRelationships) > 0 {
+		cols := []clause.Column{{Name: "component_id"}, {Name: "relationship_id"}, {Name: "selector_id"}}
+		if err := db.Gorm.Clauses(clause.OnConflict{UpdateAll: true, Columns: cols}).Create(req.ComponentRelationships).Error; err != nil {
+			return fmt.Errorf("error upserting component_relationships; %w", err)
 		}
 	}
 
@@ -54,6 +67,13 @@ func insertUpstreamMsg(ctx context.Context, req *api.PushData) error {
 		}
 	}
 
+	if len(req.ConfigComponentRelationships) > 0 {
+		cols := []clause.Column{{Name: "component_id"}, {Name: "config_id"}}
+		if err := db.Gorm.Clauses(clause.OnConflict{UpdateAll: true, Columns: cols}).Create(req.ConfigComponentRelationships).Error; err != nil {
+			return fmt.Errorf("error upserting config_component_relationships; %w", err)
+		}
+	}
+
 	if len(req.ConfigChanges) > 0 {
 		if err := db.Gorm.Clauses(clause.OnConflict{UpdateAll: true}).Create(req.ConfigChanges).Error; err != nil {
 			return fmt.Errorf("error upserting config_changes; %w", err)
@@ -63,6 +83,19 @@ func insertUpstreamMsg(ctx context.Context, req *api.PushData) error {
 	if len(req.ConfigAnalysis) > 0 {
 		if err := db.Gorm.Clauses(clause.OnConflict{UpdateAll: true}).Create(req.ConfigAnalysis).Error; err != nil {
 			return fmt.Errorf("error upserting config_analysis; %w", err)
+		}
+	}
+
+	if len(req.Checks) > 0 {
+		if err := db.Gorm.Clauses(clause.OnConflict{UpdateAll: true}).Create(req.Checks).Error; err != nil {
+			return fmt.Errorf("error upserting checks; %w", err)
+		}
+	}
+
+	if len(req.CheckStatuses) > 0 {
+		cols := []clause.Column{{Name: "check_id"}, {Name: "time"}}
+		if err := db.Gorm.Clauses(clause.OnConflict{UpdateAll: true, Columns: cols}).Create(req.CheckStatuses).Error; err != nil {
+			return fmt.Errorf("error upserting check_statuses; %w", err)
 		}
 	}
 
