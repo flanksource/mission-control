be530e4ba2d691f564da6b8d33d114ffee4cce48
Yash Mehrotra <yashmehrotra95@gmail.com>
2023-02-22 17:04:05 +0530

    chore: bump duty

--- auth/controllers.go
+++ auth/controllers.go
@@ -21,28 +21,40 @@ type InviteUserRequest struct {
 func (k *KratosHandler) InviteUser(c echo.Context) error {
 	var reqData InviteUserRequest
 	if err := c.Bind(&reqData); err != nil {
-		return c.JSON(http.StatusBadRequest, api.HTTPErrorMessage{Error: err.Error(), Message: "invalid request"})
+		return c.JSON(http.StatusBadRequest, api.HTTPError{
+			Error:   err.Error(),
+			Message: "invalid request",
+		})
 	}
 
 	identity, err := k.createUser(c.Request().Context(), reqData.FirstName, reqData.LastName, reqData.Email)
 	if err != nil {
 		// User already exists
 		if strings.Contains(err.Error(), http.StatusText(http.StatusConflict)) {
-			return c.JSON(http.StatusInternalServerError, api.HTTPErrorMessage{Error: "user already exists", Message: "error creating user"})
+			return c.JSON(http.StatusInternalServerError, api.HTTPError{
+				Error:   "user already exists",
+				Message: "error creating user",
+			})
 		}
 
-		return c.JSON(http.StatusInternalServerError, api.HTTPErrorMessage{Error: err.Error(), Message: "error creating user"})
+		return c.JSON(http.StatusInternalServerError, api.HTTPError{
+			Error:   err.Error(),
+			Message: "error creating user",
+		})
 	}
 
 	link, err := k.createRecoveryLink(c.Request().Context(), identity.Id)
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, api.HTTPErrorMessage{Error: err.Error(), Message: "error creating recovery link"})
+		return c.JSON(http.StatusInternalServerError, api.HTTPError{Error: err.Error(), Message: "error creating recovery link"})
 	}
 
 	body := fmt.Sprintf(inviteUserTemplate, reqData.FirstName, link)
 	inviteMail := mail.New(reqData.Email, "User Invite", body, "text/html")
 	if err = inviteMail.Send(); err != nil {
-		return c.JSON(http.StatusInternalServerError, api.HTTPErrorMessage{Error: err.Error(), Message: "error sending email"})
+		return c.JSON(http.StatusInternalServerError, api.HTTPError{
+			Error:   err.Error(),
+			Message: "error sending email",
+		})
 	}
 
 	respJSON := []byte(fmt.Sprintf(`{"link": "%s"}`, link))
