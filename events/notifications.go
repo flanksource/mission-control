b0d9f1262937fda72b785512e1799e239ccfa242
Aditya Thebe <thebeaditya@gmail.com>
2023-09-27 17:02:15 +0545

    chore: refactor events/notifications.go

--- events/notifications.go
+++ events/notifications.go
@@ -1,20 +1,15 @@
 package events
 
 import (
-	"encoding/json"
 	"fmt"
-	"net/url"
 	"strings"
-	"time"
 
-	"github.com/flanksource/commons/template"
-	cUtils "github.com/flanksource/commons/utils"
 	"github.com/flanksource/duty"
 	"github.com/flanksource/duty/models"
 	"github.com/flanksource/incident-commander/api"
 	"github.com/flanksource/incident-commander/db"
 	"github.com/flanksource/incident-commander/logs"
-	pkgNotification "github.com/flanksource/incident-commander/notification"
+	"github.com/flanksource/incident-commander/notification"
 	pkgResponder "github.com/flanksource/incident-commander/responder"
 	"github.com/flanksource/incident-commander/teams"
 	"github.com/flanksource/incident-commander/utils/expression"
@@ -55,216 +50,11 @@ func NewNotificationSendConsumerAsync() AsyncEventConsumer {
 	}
 }
 
-func sendNotifications(ctx api.Context, events []api.Event) []api.Event {
-	var failedEvents []api.Event
-	for _, e := range events {
-		var props NotificationEventPayload
-		props.FromMap(e.Properties)
-
-		notificationContext := pkgNotification.NewContext(ctx, props.NotificationID)
-		notificationContext.WithSource(props.EventName, props.ID)
-		logs.IfError(notificationContext.StartLog(), "error persisting start of notification send history")
-
-		if err := sendNotification(notificationContext, e); err != nil {
-			e.Error = err.Error()
-			failedEvents = append(failedEvents, e)
-			notificationContext.WithError(err.Error())
-		}
-
-		logs.IfError(notificationContext.EndLog(), "error persisting end of notification send history")
-	}
-
-	return failedEvents
-}
-
-// NotificationEventPayload holds data to create a notification
-type NotificationEventPayload struct {
-	ID               uuid.UUID  `json:"id"`                          // Resource id. depends what it is based on the original event.
-	EventName        string     `json:"event_name"`                  // The name of the original event this notification is for.
-	PersonID         *uuid.UUID `json:"person_id,omitempty"`         // The person recipient.
-	TeamID           string     `json:"team_id,omitempty"`           // The team recipient.
-	NotificationName string     `json:"notification_name,omitempty"` // Name of the notification of a team or a custom service of the notification.
-	NotificationID   uuid.UUID  `json:"notification_id,omitempty"`   // ID of the notification.
-	EventCreatedAt   time.Time  `json:"event_created_at"`            // Timestamp at which the original event was created
-}
-
-// NotificationTemplate holds in data for notification
-// that'll be used by struct templater.
-type NotificationTemplate struct {
-	Title      string            `template:"true"`
-	Message    string            `template:"true"`
-	Properties map[string]string `template:"true"`
-}
-
-func (t *NotificationEventPayload) AsMap() map[string]string {
-	m := make(map[string]string)
-	b, _ := json.Marshal(&t)
-	_ = json.Unmarshal(b, &m)
-	return m
-}
-
-func (t *NotificationEventPayload) FromMap(m map[string]string) {
-	b, _ := json.Marshal(m)
-	_ = json.Unmarshal(b, &t)
-}
-
-// labelsTemplate is a helper func to generate the template for displaying labels
-func labelsTemplate(field string) string {
-	return fmt.Sprintf("{{if %s}}### Labels: \n{{range $k, $v := %s}}**{{$k}}**: {{$v}} \n{{end}}{{end}}", field, field)
-}
-
-// defaultTitleAndBody returns the default title and body for notification
-// based on the given event.
-func defaultTitleAndBody(event string) (title string, body string) {
-	switch event {
-	case EventCheckPassed:
-		title = "Check {{.check.name}} has passed"
-		body = fmt.Sprintf(`Canary: {{.canary.name}}
-{{if .agent}}Agent: {{.agent.name}}{{end}}
-{{if .status.message}}Message: {{.status.message}} {{end}}
-%s
-
-[Reference]({{.permalink}})`, labelsTemplate(".check.labels"))
-
-	case EventCheckFailed:
-		title = "Check {{.check.name}} has failed"
-		body = fmt.Sprintf(`Canary: {{.canary.name}}
-{{if .agent}}Agent: {{.agent.name}}{{end}}
-Error: {{.status.error}}
-%s
-
-[Reference]({{.permalink}})`, labelsTemplate(".check.labels"))
-
-	case EventComponentStatusHealthy, EventComponentStatusUnhealthy, EventComponentStatusInfo, EventComponentStatusWarning, EventComponentStatusError:
-		title = "Component {{.component.name}} status updated to {{.component.status}}"
-		body = fmt.Sprintf("%s\n[Reference]({{.permalink}})", labelsTemplate(".component.labels"))
-
-	case EventIncidentCommentAdded:
-		title = "{{.author.name}} left a comment on {{.incident.incident_id}}: {{.incident.title}}"
-		body = "{{.comment.comment}}\n\n[Reference]({{.permalink}})"
-
-	case EventIncidentCreated:
-		title = "{{.incident.incident_id}}: {{.incident.title}} ({{.incident.severity}}) created"
-		body = "Type: {{.incident.type}}\n\n[Reference]({{.permalink}})"
-
-	case EventIncidentDODAdded:
-		title = "Definition of Done added | {{.incident.incident_id}}: {{.incident.title}}"
-		body = "Evidence: {{.evidence.description}}\n\n[Reference]({{.permalink}})"
-
-	case EventIncidentDODPassed, EventIncidentDODRegressed:
-		title = "Definition of Done {{if .evidence.done}}passed{{else}}regressed{{end}} | {{.incident.incident_id}}: {{.incident.title}}"
-		body = `Evidence: {{.evidence.description}}
-Hypothesis: {{.hypothesis.title}}
-
-[Reference]({{.permalink}})`
-
-	case EventIncidentResponderAdded:
-		title = "New responder added to {{.incident.incident_id}}: {{.incident.title}}"
-		body = "Responder {{.responder.name}}\n\n[Reference]({{.permalink}})"
-
-	case EventIncidentResponderRemoved:
-		title = "Responder removed from {{.incident.incident_id}}: {{.incident.title}}"
-		body = "Responder {{.responder.name}}\n\n[Reference]({{.permalink}})"
-
-	case EventIncidentStatusCancelled, EventIncidentStatusClosed, EventIncidentStatusInvestigating, EventIncidentStatusMitigated, EventIncidentStatusOpen, EventIncidentStatusResolved:
-		title = "{{.incident.title}} status updated"
-		body = "New Status: {{.incident.status}}\n\n[Reference]({{.permalink}})"
-
-	case EventPlaybookSpecApprovalUpdated, EventPlaybookApprovalInserted:
-		// Not applicable
-	}
-
-	return title, body
-}
-
-func sendNotification(ctx *pkgNotification.Context, event api.Event) error {
-	var props NotificationEventPayload
-	props.FromMap(event.Properties)
-
-	originalEvent := api.Event{Name: props.EventName, CreatedAt: props.EventCreatedAt}
-	celEnv, err := getEnvForEvent(ctx.Context, originalEvent, event.Properties)
-	if err != nil {
-		return err
-	}
-
-	templater := template.StructTemplater{
-		Values:         celEnv,
-		ValueFunctions: true,
-		DelimSets: []template.Delims{
-			{Left: "{{", Right: "}}"},
-			{Left: "$(", Right: ")"},
-		},
-	}
-
-	notification, err := pkgNotification.GetNotification(ctx.Context, props.NotificationID.String())
-	if err != nil {
-		return err
-	}
-
-	defaultTitle, defaultBody := defaultTitleAndBody(props.EventName)
-
-	data := NotificationTemplate{
-		Title:      cUtils.Coalesce(notification.Title, defaultTitle),
-		Message:    cUtils.Coalesce(notification.Template, defaultBody),
-		Properties: notification.Properties,
-	}
-
-	if err := templater.Walk(&data); err != nil {
-		return fmt.Errorf("error templating notification: %w", err)
-	}
-
-	if props.PersonID != nil {
-		ctx.WithPersonID(props.PersonID).WithRecipientType(pkgNotification.RecipientTypePerson)
-		var emailAddress string
-		if err := ctx.DB().Model(&models.Person{}).Select("email").Where("id = ?", props.PersonID).Find(&emailAddress).Error; err != nil {
-			return fmt.Errorf("failed to get email of person(id=%s); %v", props.PersonID, err)
-		}
-
-		smtpURL := fmt.Sprintf("%s?ToAddresses=%s", api.SystemSMTP, url.QueryEscape(emailAddress))
-		return pkgNotification.Send(ctx, "", smtpURL, data.Title, data.Message, data.Properties)
-	}
-
-	if props.TeamID != "" {
-		ctx.WithRecipientType(pkgNotification.RecipientTypeTeam)
-		teamSpec, err := teams.GetTeamSpec(ctx.Context, props.TeamID)
-		if err != nil {
-			return fmt.Errorf("failed to get team(id=%s); %v", props.TeamID, err)
-		}
-
-		for _, cn := range teamSpec.Notifications {
-			if cn.Name != props.NotificationName {
-				continue
-			}
-
-			if err := templater.Walk(&cn); err != nil {
-				return fmt.Errorf("error templating notification: %w", err)
-			}
-
-			return pkgNotification.Send(ctx, cn.Connection, cn.URL, data.Title, data.Message, data.Properties, cn.Properties)
-		}
-	}
-
-	for _, cn := range notification.CustomNotifications {
-		ctx.WithRecipientType(pkgNotification.RecipientTypeCustom)
-		if cn.Name != props.NotificationName {
-			continue
-		}
-
-		if err := templater.Walk(&cn); err != nil {
-			return fmt.Errorf("error templating notification: %w", err)
-		}
-
-		return pkgNotification.Send(ctx, cn.Connection, cn.URL, data.Title, data.Message, data.Properties, cn.Properties)
-	}
-
-	return nil
-}
-
-// addNotificationEvent responds to a event that can possible generate a notification.
+// addNotificationEvent responds to a event that can possibly generate a notification.
 // If a notification is found for the given event and passes all the filters, then
-// a new notification event is created.
+// a new `notification.send` event is created.
 func addNotificationEvent(ctx api.Context, event api.Event) error {
-	notificationIDs, err := pkgNotification.GetNotificationIDsForEvent(ctx, event.Name)
+	notificationIDs, err := notification.GetNotificationIDsForEvent(ctx, event.Name)
 	if err != nil {
 		return err
 	}
@@ -279,7 +69,7 @@ func addNotificationEvent(ctx api.Context, event api.Event) error {
 	}
 
 	for _, id := range notificationIDs {
-		n, err := pkgNotification.GetNotification(ctx, id)
+		n, err := notification.GetNotification(ctx, id)
 		if err != nil {
 			return err
 		}
@@ -299,13 +89,13 @@ func addNotificationEvent(ctx api.Context, event api.Event) error {
 			continue
 		}
 
-		if n.PersonID != nil {
-			resourceID, err := uuid.Parse(event.Properties["id"])
-			if err != nil {
-				return fmt.Errorf("failed to parse resource id: %v", err)
-			}
+		resourceID, err := uuid.Parse(event.Properties["id"])
+		if err != nil {
+			return fmt.Errorf("failed to parse resource id: %v", err)
+		}
 
-			prop := NotificationEventPayload{
+		if n.PersonID != nil {
+			payload := notification.NotificationEventPayload{
 				EventName:      event.Name,
 				NotificationID: n.ID,
 				ID:             resourceID,
@@ -315,7 +105,7 @@ func addNotificationEvent(ctx api.Context, event api.Event) error {
 
 			newEvent := &api.Event{
 				Name:       EventNotificationSend,
-				Properties: prop.AsMap(),
+				Properties: payload.AsMap(),
 			}
 			if err := ctx.DB().Clauses(eventQueueOnConflictClause).Create(newEvent).Error; err != nil {
 				return fmt.Errorf("failed to create notification event for person(id=%s): %v", n.PersonID, err)
@@ -335,22 +125,18 @@ func addNotificationEvent(ctx api.Context, event api.Event) error {
 					continue
 				}
 
-				resourceID, err := uuid.Parse(event.Properties["id"])
-				if err != nil {
-					return fmt.Errorf("failed to parse resource id: %v", err)
-				}
-
-				prop := NotificationEventPayload{
+				payload := notification.NotificationEventPayload{
 					EventName:        event.Name,
 					NotificationID:   n.ID,
 					ID:               resourceID,
 					TeamID:           n.TeamID.String(),
 					NotificationName: cn.Name,
+					EventCreatedAt:   event.CreatedAt,
 				}
 
 				newEvent := &api.Event{
 					Name:       EventNotificationSend,
-					Properties: prop.AsMap(),
+					Properties: payload.AsMap(),
 				}
 
 				if err := ctx.DB().Clauses(eventQueueOnConflictClause).Create(newEvent).Error; err != nil {
@@ -366,21 +152,17 @@ func addNotificationEvent(ctx api.Context, event api.Event) error {
 				continue
 			}
 
-			resourceID, err := uuid.Parse(event.Properties["id"])
-			if err != nil {
-				return fmt.Errorf("failed to parse resource id: %v", err)
-			}
-
-			prop := NotificationEventPayload{
+			payload := notification.NotificationEventPayload{
 				EventName:        event.Name,
 				NotificationID:   n.ID,
 				ID:               resourceID,
 				NotificationName: cn.Name,
+				EventCreatedAt:   event.CreatedAt,
 			}
 
 			newEvent := &api.Event{
 				Name:       EventNotificationSend,
-				Properties: prop.AsMap(),
+				Properties: payload.AsMap(),
 			}
 
 			if err := ctx.DB().Clauses(eventQueueOnConflictClause).Create(newEvent).Error; err != nil {
@@ -390,6 +172,39 @@ func addNotificationEvent(ctx api.Context, event api.Event) error {
 	}
 
 	return nil
+}
+
+// sendNotifications sends a notification for each of the given events - one at a time.
+// It returns any events that failed to send.
+func sendNotifications(ctx api.Context, events []api.Event) []api.Event {
+	var failedEvents []api.Event
+	for _, e := range events {
+		var payload notification.NotificationEventPayload
+		payload.FromMap(e.Properties)
+
+		notificationContext := notification.NewContext(ctx, payload.NotificationID)
+		notificationContext.WithSource(payload.EventName, payload.ID)
+		logs.IfError(notificationContext.StartLog(), "error persisting start of notification send history")
+
+		originalEvent := api.Event{Name: payload.EventName, CreatedAt: payload.EventCreatedAt}
+		celEnv, err := getEnvForEvent(ctx, originalEvent, e.Properties)
+		if err != nil {
+			e.Error = err.Error()
+			failedEvents = append(failedEvents, e)
+			notificationContext.WithError(err.Error())
+			logs.IfError(notificationContext.EndLog(), "error persisting end of notification send history")
+		}
+
+		if err := notification.SendNotification(notificationContext, payload, celEnv); err != nil {
+			e.Error = err.Error()
+			failedEvents = append(failedEvents, e)
+			notificationContext.WithError(err.Error())
+		}
+
+		logs.IfError(notificationContext.EndLog(), "error persisting end of notification send history")
+	}
+
+	return failedEvents
 }
 
 // getEnvForEvent gets the environment variables for the given event
---
+++ notification/events.go
@@ -0,0 +1,214 @@
+package notification
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/url"
+	"time"
+
+	"github.com/flanksource/commons/template"
+	"github.com/flanksource/commons/utils"
+	"github.com/flanksource/duty/models"
+	"github.com/google/uuid"
+
+	"github.com/flanksource/incident-commander/api"
+	"github.com/flanksource/incident-commander/teams"
+)
+
+// List of all events that can create notifications ...
+const (
+	EventCheckPassed = "check.passed"
+	EventCheckFailed = "check.failed"
+
+	EventComponentStatusHealthy   = "component.status.healthy"
+	EventComponentStatusUnhealthy = "component.status.unhealthy"
+	EventComponentStatusInfo      = "component.status.info"
+	EventComponentStatusWarning   = "component.status.warning"
+	EventComponentStatusError     = "component.status.error"
+
+	EventIncidentCommentAdded        = "incident.comment.added"
+	EventIncidentCreated             = "incident.created"
+	EventIncidentDODAdded            = "incident.dod.added"
+	EventIncidentDODPassed           = "incident.dod.passed"
+	EventIncidentDODRegressed        = "incident.dod.regressed"
+	EventIncidentResponderAdded      = "incident.responder.added"
+	EventIncidentResponderRemoved    = "incident.responder.removed"
+	EventIncidentStatusCancelled     = "incident.status.cancelled"
+	EventIncidentStatusClosed        = "incident.status.closed"
+	EventIncidentStatusInvestigating = "incident.status.investigating"
+	EventIncidentStatusMitigated     = "incident.status.mitigated"
+	EventIncidentStatusOpen          = "incident.status.open"
+	EventIncidentStatusResolved      = "incident.status.resolved"
+)
+
+// NotificationTemplate holds in data for notification
+// that'll be used by struct templater.
+type NotificationTemplate struct {
+	Title      string            `template:"true"`
+	Message    string            `template:"true"`
+	Properties map[string]string `template:"true"`
+}
+
+// NotificationEventPayload holds data to create a notification.
+type NotificationEventPayload struct {
+	ID               uuid.UUID  `json:"id"`                          // Resource id. depends what it is based on the original event.
+	EventName        string     `json:"event_name"`                  // The name of the original event this notification is for.
+	PersonID         *uuid.UUID `json:"person_id,omitempty"`         // The person recipient.
+	TeamID           string     `json:"team_id,omitempty"`           // The team recipient.
+	NotificationName string     `json:"notification_name,omitempty"` // Name of the notification of a team or a custom service of the notification.
+	NotificationID   uuid.UUID  `json:"notification_id,omitempty"`   // ID of the notification.
+	EventCreatedAt   time.Time  `json:"event_created_at"`            // Timestamp at which the original event was created
+}
+
+func (t *NotificationEventPayload) AsMap() map[string]string {
+	m := make(map[string]string)
+	b, _ := json.Marshal(&t)
+	_ = json.Unmarshal(b, &m)
+	return m
+}
+
+func (t *NotificationEventPayload) FromMap(m map[string]string) {
+	b, _ := json.Marshal(m)
+	_ = json.Unmarshal(b, &t)
+}
+
+// SendNotification generates the notification from the given event and sends it.
+func SendNotification(ctx *Context, payload NotificationEventPayload, celEnv map[string]any) error {
+	templater := template.StructTemplater{
+		Values:         celEnv,
+		ValueFunctions: true,
+		DelimSets: []template.Delims{
+			{Left: "{{", Right: "}}"},
+			{Left: "$(", Right: ")"},
+		},
+	}
+
+	notification, err := GetNotification(ctx.Context, payload.NotificationID.String())
+	if err != nil {
+		return err
+	}
+
+	defaultTitle, defaultBody := defaultTitleAndBody(payload.EventName)
+
+	data := NotificationTemplate{
+		Title:      utils.Coalesce(notification.Title, defaultTitle),
+		Message:    utils.Coalesce(notification.Template, defaultBody),
+		Properties: notification.Properties,
+	}
+
+	if err := templater.Walk(&data); err != nil {
+		return fmt.Errorf("error templating notification: %w", err)
+	}
+
+	if payload.PersonID != nil {
+		ctx.WithPersonID(payload.PersonID).WithRecipientType(RecipientTypePerson)
+		var emailAddress string
+		if err := ctx.DB().Model(&models.Person{}).Select("email").Where("id = ?", payload.PersonID).Find(&emailAddress).Error; err != nil {
+			return fmt.Errorf("failed to get email of person(id=%s); %v", payload.PersonID, err)
+		}
+
+		smtpURL := fmt.Sprintf("%s?ToAddresses=%s", api.SystemSMTP, url.QueryEscape(emailAddress))
+		return Send(ctx, "", smtpURL, data.Title, data.Message, data.Properties)
+	}
+
+	if payload.TeamID != "" {
+		ctx.WithRecipientType(RecipientTypeTeam)
+		teamSpec, err := teams.GetTeamSpec(ctx.Context, payload.TeamID)
+		if err != nil {
+			return fmt.Errorf("failed to get team(id=%s); %v", payload.TeamID, err)
+		}
+
+		for _, cn := range teamSpec.Notifications {
+			if cn.Name != payload.NotificationName {
+				continue
+			}
+
+			if err := templater.Walk(&cn); err != nil {
+				return fmt.Errorf("error templating notification: %w", err)
+			}
+
+			return Send(ctx, cn.Connection, cn.URL, data.Title, data.Message, data.Properties, cn.Properties)
+		}
+	}
+
+	for _, cn := range notification.CustomNotifications {
+		ctx.WithRecipientType(RecipientTypeCustom)
+		if cn.Name != payload.NotificationName {
+			continue
+		}
+
+		if err := templater.Walk(&cn); err != nil {
+			return fmt.Errorf("error templating notification: %w", err)
+		}
+
+		return Send(ctx, cn.Connection, cn.URL, data.Title, data.Message, data.Properties, cn.Properties)
+	}
+
+	return nil
+}
+
+// labelsTemplate is a helper func to generate the template for displaying labels
+func labelsTemplate(field string) string {
+	return fmt.Sprintf("{{if %s}}### Labels: \n{{range $k, $v := %s}}**{{$k}}**: {{$v}} \n{{end}}{{end}}", field, field)
+}
+
+// defaultTitleAndBody returns the default title and body for notification
+// based on the given event.
+func defaultTitleAndBody(event string) (title string, body string) {
+	switch event {
+	case EventCheckPassed:
+		title = "Check {{.check.name}} has passed"
+		body = fmt.Sprintf(`Canary: {{.canary.name}}
+{{if .agent}}Agent: {{.agent.name}}{{end}}
+{{if .status.message}}Message: {{.status.message}} {{end}}
+%s
+
+[Reference]({{.permalink}})`, labelsTemplate(".check.labels"))
+
+	case EventCheckFailed:
+		title = "Check {{.check.name}} has failed"
+		body = fmt.Sprintf(`Canary: {{.canary.name}}
+{{if .agent}}Agent: {{.agent.name}}{{end}}
+Error: {{.status.error}}
+%s
+
+[Reference]({{.permalink}})`, labelsTemplate(".check.labels"))
+
+	case EventComponentStatusHealthy, EventComponentStatusUnhealthy, EventComponentStatusInfo, EventComponentStatusWarning, EventComponentStatusError:
+		title = "Component {{.component.name}} status updated to {{.component.status}}"
+		body = fmt.Sprintf("%s\n[Reference]({{.permalink}})", labelsTemplate(".component.labels"))
+
+	case EventIncidentCommentAdded:
+		title = "{{.author.name}} left a comment on {{.incident.incident_id}}: {{.incident.title}}"
+		body = "{{.comment.comment}}\n\n[Reference]({{.permalink}})"
+
+	case EventIncidentCreated:
+		title = "{{.incident.incident_id}}: {{.incident.title}} ({{.incident.severity}}) created"
+		body = "Type: {{.incident.type}}\n\n[Reference]({{.permalink}})"
+
+	case EventIncidentDODAdded:
+		title = "Definition of Done added | {{.incident.incident_id}}: {{.incident.title}}"
+		body = "Evidence: {{.evidence.description}}\n\n[Reference]({{.permalink}})"
+
+	case EventIncidentDODPassed, EventIncidentDODRegressed:
+		title = "Definition of Done {{if .evidence.done}}passed{{else}}regressed{{end}} | {{.incident.incident_id}}: {{.incident.title}}"
+		body = `Evidence: {{.evidence.description}}
+Hypothesis: {{.hypothesis.title}}
+
+[Reference]({{.permalink}})`
+
+	case EventIncidentResponderAdded:
+		title = "New responder added to {{.incident.incident_id}}: {{.incident.title}}"
+		body = "Responder {{.responder.name}}\n\n[Reference]({{.permalink}})"
+
+	case EventIncidentResponderRemoved:
+		title = "Responder removed from {{.incident.incident_id}}: {{.incident.title}}"
+		body = "Responder {{.responder.name}}\n\n[Reference]({{.permalink}})"
+
+	case EventIncidentStatusCancelled, EventIncidentStatusClosed, EventIncidentStatusInvestigating, EventIncidentStatusMitigated, EventIncidentStatusOpen, EventIncidentStatusResolved:
+		title = "{{.incident.title}} status updated"
+		body = "New Status: {{.incident.status}}\n\n[Reference]({{.permalink}})"
+	}
+
+	return title, body
+}
