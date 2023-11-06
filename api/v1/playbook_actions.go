d447550264958af2084d05eb8657d15160199bc0
Moshe Immermam <moshe@flanksource.com>
2023-11-06 14:07:40 +0200

    fix: connections should use the namespace they were created in

--- api/v1/playbook_actions.go
+++ api/v1/playbook_actions.go
@@ -11,7 +11,6 @@ import (
 	"github.com/flanksource/duty"
 	"github.com/flanksource/duty/models"
 	"github.com/flanksource/duty/types"
-	"github.com/flanksource/incident-commander/api"
 	"k8s.io/client-go/kubernetes"
 )
 
@@ -84,8 +83,8 @@ type ExecConnections struct {
 
 type connectionContext interface {
 	gocontext.Context
-	HydratedConnectionByURL(namespace, connectionName string) (*models.Connection, error)
-	GetEnvValueFromCache(env types.EnvVar, namespace string) (string, error)
+	HydrateConnectionByURL(connectionName string) (*models.Connection, error)
+	GetEnvValueFromCache(env types.EnvVar, namespace ...string) (string, error)
 }
 
 type GCPConnection struct {
@@ -98,7 +97,7 @@ type GCPConnection struct {
 // HydrateConnection attempts to find the connection by name
 // and populate the endpoint and credentials.
 func (g *GCPConnection) HydrateConnection(ctx connectionContext) error {
-	connection, err := ctx.HydratedConnectionByURL(api.Namespace, g.ConnectionName)
+	connection, err := ctx.HydrateConnectionByURL(g.ConnectionName)
 	if err != nil {
 		return err
 	}
@@ -121,7 +120,7 @@ type AzureConnection struct {
 // HydrateConnection attempts to find the connection by name
 // and populate the endpoint and credentials.
 func (g *AzureConnection) HydrateConnection(ctx connectionContext) error {
-	connection, err := ctx.HydratedConnectionByURL(api.Namespace, g.ConnectionName)
+	connection, err := ctx.HydrateConnectionByURL(g.ConnectionName)
 	if err != nil {
 		return err
 	}
@@ -155,7 +154,7 @@ type AWSConnection struct {
 // If a connection name is specified, it'll be used to populate the endpoint, accessKey and secretKey.
 func (t *AWSConnection) Populate(ctx connectionContext, k8s kubernetes.Interface, namespace string) error {
 	if t.ConnectionName != "" {
-		connection, err := ctx.HydratedConnectionByURL(namespace, t.ConnectionName)
+		connection, err := ctx.HydrateConnectionByURL(t.ConnectionName)
 		if err != nil {
 			return fmt.Errorf("could not parse EC2 access key: %v", err)
 		}
