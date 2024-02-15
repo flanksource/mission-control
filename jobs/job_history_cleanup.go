c9cc3301d269c2a60e6ea9170339a926d44a5fd8
Yash Mehrotra <yashmehrotra95@gmail.com>
2024-02-15 16:57:20 +0530

    fix: correct success count in job history cleanup

--- jobs/job_history_cleanup.go
+++ jobs/job_history_cleanup.go
@@ -6,6 +6,6 @@ import (
 
 func CleanupJobHistoryTable(ctx job.JobRuntime) error {
 	res := ctx.DB().Exec("DELETE FROM job_history where now() - created_at > interval '30 day'")
-	ctx.History.ErrorCount = int(res.RowsAffected)
+	ctx.History.SuccessCount = int(res.RowsAffected)
 	return res.Error
 }
