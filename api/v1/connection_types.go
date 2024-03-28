4eab97aa16040e6eb3a1ab806f90e85ce06cb356
Aditya Thebe <contact@adityathebe.com>
2024-03-28 11:31:55 +0545

    new connection spec (#927)
    * feat: new connection spec
    
    * changes to database connections
    
    * fix: remove ssh field from git connection
    
    * fix: mssql insecure tls

--- api/v1/connection_types.go
+++ api/v1/connection_types.go
@@ -5,16 +5,255 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+type ConnectionTelegram struct {
+	Token types.EnvVar `json:"token"`
+
+	// Chat ID or Channel name (using @channel-name)
+	Chats types.EnvVar `json:"chats"`
+}
+
+type ConnectionSlack struct {
+	Token types.EnvVar `json:"token"`
+
+	// Channel to send messages to in Cxxxxxxxxxx format
+	Channel string `json:"channel"`
+
+	BotName string `json:"botname"`
+}
+
+type ConnectionNtfy struct {
+	Topic string `json:"topic"`
+
+	Host     string       `json:"host"`
+	Username types.EnvVar `json:"username"`
+	Password types.EnvVar `json:"password"`
+}
+
+type ConnectionDiscord struct {
+	Token     string `json:"token"`
+	WebhookID string `json:"webhookID"`
+}
+
+type ConnectionSMTP struct {
+	Host     string       `json:"host"`
+	Username types.EnvVar `json:"username,omitempty"`
+	Password types.EnvVar `json:"password,omitempty"`
+
+	// Default: false
+	InsecureTLS bool `json:"insecureTLS,omitempty"`
+
+	// Encryption Method
+	// 	Defulat: auto
+	// 	Possible values: None, ExplicitTLS, ImplicitTLS, Auto
+	Encryption string `json:"encryption,omitempty"`
+
+	// SMTP server port
+	// 	Default: 587
+	Port int `json:"port,omitempty"`
+
+	// Email address that the mail are sent from
+	FromAddress string `json:"fromAddress"`
+
+	// List of recipient e-mails
+	ToAddresses []string `json:"toAddresses,omitempty"`
+
+	// The subject of the sent mail
+	Subject string `json:"subject,omitempty"`
+
+	// Auth - SMTP authentication method
+	// Possible values: None, Plain, CRAMMD5, Unknown, OAuth2
+	Auth string `json:"auth,omitempty"`
+}
+
+type ConnectionPushbullet struct {
+	Token types.EnvVar `json:"token"`
+
+	Targets []string `json:"targets"`
+}
+
+type ConnectionPushover struct {
+	Token types.EnvVar `json:"token"`
+	// User key
+	User string `json:"user"`
+}
+
+type ConnectionPostgres struct {
+	// URL is the connection url.
+	URL types.EnvVar `json:"url,omitempty"`
+
+	// <host:port>
+	Host        types.EnvVar `json:"host,omitempty"`
+	Username    types.EnvVar `json:"username,omitempty"`
+	Password    types.EnvVar `json:"password,omitempty"`
+	Database    types.EnvVar `json:"database,omitempty"`
+	InsecureTLS bool         `json:"insecureTLS,omitempty"`
+}
+
+type ConnectionMySQL struct {
+	// URL is the connection url.
+	URL types.EnvVar `json:"url,omitempty"`
+
+	// <host:port>
+	Host        types.EnvVar `json:"host,omitempty"`
+	Username    types.EnvVar `json:"username,omitempty"`
+	Password    types.EnvVar `json:"password,omitempty"`
+	Database    types.EnvVar `json:"database,omitempty"`
+	InsecureTLS bool         `json:"insecureTLS,omitempty"`
+}
+
+type ConnectionMSSQL struct {
+	// URL is the connection url.
+	URL types.EnvVar `json:"url,omitempty"`
+
+	// <host:port>
+	Host                   types.EnvVar `json:"host,omitempty"`
+	Username               types.EnvVar `json:"username,omitempty"`
+	Password               types.EnvVar `json:"password,omitempty"`
+	Database               types.EnvVar `json:"database,omitempty"`
+	TrustServerCertificate bool         `json:"trustServerCertificate,omitempty"`
+}
+
+type ConnectionMongo struct {
+	// URL is the connection url.
+	URL types.EnvVar `json:"url,omitempty"`
+
+	// <host:port>
+	Host        types.EnvVar `json:"host,omitempty"`
+	Username    types.EnvVar `json:"username,omitempty"`
+	Password    types.EnvVar `json:"password,omitempty"`
+	Database    types.EnvVar `json:"database,omitempty"`
+	ReplicaSet  string       `json:"replicaSet,omitempty"`
+	InsecureTLS bool         `json:"insecureTLS,omitempty"`
+}
+
+type ConnectionAWSS3 struct {
+	ConnectionAWS `json:",inline"`
+	Bucket        string `json:"bucket"`
+}
+
+type ConnectionAWS struct {
+	Region      string       `json:"region,omitempty"`
+	Profile     string       `json:"profile,omitempty"`
+	InsecureTLS bool         `json:"insecureTLS,omitempty"`
+	AccessKey   types.EnvVar `json:"accessKey,omitempty"`
+	SecretKey   types.EnvVar `json:"secretKey,omitempty"`
+}
+
+type ConnectionAzure struct {
+	ClientID     types.EnvVar `json:"clientID"`
+	ClientSecret types.EnvVar `json:"clientSecret,omitempty"`
+	TenantID     types.EnvVar `json:"tenantID"`
+}
+
+type ConnectionAzureDevops struct {
+	Organization        string       `json:"organization"`
+	PersonalAccessToken types.EnvVar `json:"personalAccessToken"`
+}
+
+type ConnectionGCP struct {
+	Endpoint    types.EnvVar `json:"endpoint,omitempty"`
+	Certificate types.EnvVar `json:"certificate,omitempty"`
+}
+
+type ConnectionFolder struct {
+	Path string `json:"path"`
+}
+
+type ConnectionKubernetes struct {
+	Certificate types.EnvVar `json:"certificate"`
+}
+
+type ConnectionGitHub struct {
+	PersonalAccessToken types.EnvVar `json:"personalAccessToken"`
+}
+
+type ConnectionGit struct {
+	URL         string        `json:"string"`
+	Ref         string        `json:"ref"`
+	Certificate *types.EnvVar `json:"certificate,omitempty"`
+	Username    *types.EnvVar `json:"username,omitempty"`
+	Password    *types.EnvVar `json:"password,omitempty"`
+}
+
+type ConnectionHTTP struct {
+	URL         string        `json:"url"`
+	InsecureTLS bool          `json:"insecureTLS,omitempty"`
+	Username    *types.EnvVar `json:"username,omitempty"`
+	Password    *types.EnvVar `json:"password,omitempty"`
+}
+
+type ConnectionSFTP struct {
+	Host     types.EnvVar `json:"host"`
+	Username types.EnvVar `json:"username"`
+	Password types.EnvVar `json:"password"`
+
+	// SMTP server port
+	// 	Default: 22
+	Port int `json:"port,omitempty"`
+
+	Path string `json:"path"`
+}
+
+type ConnectionSMB struct {
+	Server   types.EnvVar `json:"server"`
+	Username types.EnvVar `json:"username"`
+	Password types.EnvVar `json:"password"`
+
+	// SMB server port
+	// 	Default: 445
+	Port types.EnvVar `json:"port,omitempty"`
+
+	Share string `json:"share"`
+}
+
 // ConnectionSpec defines the desired state of Connection
 type ConnectionSpec struct {
-	URL         types.EnvVar        `json:"url,omitempty"`
-	Port        types.EnvVar        `json:"port,omitempty"`
-	Type        string              `json:"type,omitempty"`
-	Username    types.EnvVar        `json:"username,omitempty"`
-	Password    types.EnvVar        `json:"password,omitempty"`
-	Certificate types.EnvVar        `json:"certificate,omitempty"`
-	Properties  types.JSONStringMap `json:"properties,omitempty"`
-	InsecureTLS bool                `json:"insecure_tls,omitempty"`
+	Properties types.JSONStringMap `json:"properties,omitempty"`
+
+	AWS         *ConnectionAWS         `json:"aws,omitempty"`
+	S3          *ConnectionAWSS3       `json:"s3,omitempty"`
+	Azure       *ConnectionAzure       `json:"azure,omitempty"`
+	AzureDevops *ConnectionAzureDevops `json:"azureDevops,omitempty"`
+	GCP         *ConnectionGCP         `json:"gcp,omitempty"`
+
+	Folder     *ConnectionFolder     `json:"folder,omitempty"`
+	Git        *ConnectionGit        `json:"git,omitempty"`
+	GitHub     *ConnectionGitHub     `json:"github,omitempty"`
+	HTTP       *ConnectionHTTP       `json:"http,omitempty"`
+	Kubernetes *ConnectionKubernetes `json:"kubernetes,omitempty"`
+	MSSQL      *ConnectionMSSQL      `json:"mssql,omitempty"`
+	Mongo      *ConnectionMongo      `json:"mongo,omitempty"`
+	MySQL      *ConnectionMySQL      `json:"mysql,omitempty"`
+	Postgres   *ConnectionPostgres   `json:"postgres,omitempty"`
+	SFTP       *ConnectionSFTP       `json:"sftp,omitempty"`
+	SMB        *ConnectionSMB        `json:"smb,omitempty"`
+
+	//////////////////////////////
+	// Notification Connections //
+	//////////////////////////////
+
+	Discord    *ConnectionDiscord    `json:"discord,omitempty"`
+	Ntfy       *ConnectionNtfy       `json:"ntfy,omitempty"`
+	Pushbullet *ConnectionPushbullet `json:"pushbullet,omitempty"`
+	Pushover   *ConnectionPushover   `json:"pushover,omitempty"`
+	SMTP       *ConnectionSMTP       `json:"smtp,omitempty"`
+	Slack      *ConnectionSlack      `json:"slack,omitempty"`
+	Telegram   *ConnectionTelegram   `json:"telegram,omitempty"`
+
+	// DEPRECATED
+	URL types.EnvVar `json:"url,omitempty"`
+	// DEPRECATED
+	Port types.EnvVar `json:"port,omitempty"`
+	// DEPRECATED
+	Type string `json:"type,omitempty"`
+	// DEPRECATED
+	Username types.EnvVar `json:"username,omitempty"`
+	// DEPRECATED
+	Password types.EnvVar `json:"password,omitempty"`
+	// DEPRECATED
+	Certificate types.EnvVar `json:"certificate,omitempty"`
+	// DEPRECATED
+	InsecureTLS bool `json:"insecure_tls,omitempty"`
 }
 
 // ConnectionStatus defines the observed state of Connection
--- db/connections.go
+++ db/connections.go
@@ -1,6 +1,10 @@
 package db
 
 import (
+	"fmt"
+	"net/url"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/flanksource/duty/context"
@@ -25,6 +29,268 @@ func PersistConnectionFromCRD(ctx context.Context, obj *v1.Connection) error {
 		// Gorm.Save does not use defaults when inserting
 		// and the timestamp used is zero time
 		CreatedAt: time.Now(),
+	}
+
+	if obj.Spec.AWS != nil {
+		dbObj.Type = models.ConnectionTypeAWS
+		dbObj.Username = obj.Spec.AWS.AccessKey.String()
+		dbObj.Password = obj.Spec.AWS.SecretKey.String()
+		dbObj.Properties = map[string]string{
+			"region":       obj.Spec.AWS.Region,
+			"profile":      obj.Spec.AWS.Profile,
+			"insecure_tls": strconv.FormatBool(obj.Spec.AWS.InsecureTLS),
+		}
+	}
+
+	if obj.Spec.S3 != nil {
+		dbObj.Type = models.ConnectionTypeS3
+		dbObj.Username = obj.Spec.S3.AccessKey.String()
+		dbObj.Password = obj.Spec.S3.SecretKey.String()
+		dbObj.Properties = map[string]string{
+			"bucket":       obj.Spec.S3.Bucket,
+			"region":       obj.Spec.S3.Region,
+			"profile":      obj.Spec.S3.Profile,
+			"insecure_tls": strconv.FormatBool(obj.Spec.S3.InsecureTLS),
+		}
+	}
+
+	if obj.Spec.Azure != nil {
+		dbObj.Type = models.ConnectionTypeAzure
+		dbObj.Username = obj.Spec.Azure.ClientID.String()
+		dbObj.Password = obj.Spec.Azure.ClientSecret.String()
+		dbObj.Properties = map[string]string{
+			"tenant": obj.Spec.Azure.TenantID.String(),
+		}
+	}
+
+	if obj.Spec.GCP != nil {
+		dbObj.Type = models.ConnectionTypeGCP
+		dbObj.Certificate = obj.Spec.GCP.Certificate.String()
+		dbObj.URL = obj.Spec.GCP.Endpoint.String()
+	}
+
+	if obj.Spec.AzureDevops != nil {
+		dbObj.Type = models.ConnectionTypeAzureDevops
+		dbObj.Username = obj.Spec.AzureDevops.Organization
+		dbObj.Password = obj.Spec.AzureDevops.PersonalAccessToken.String()
+	}
+
+	if obj.Spec.Folder != nil {
+		dbObj.Type = models.ConnectionTypeFolder
+		dbObj.Properties = map[string]string{
+			"path": obj.Spec.Folder.Path,
+		}
+	}
+
+	if obj.Spec.Git != nil {
+		dbObj.Type = models.ConnectionTypeGit
+		dbObj.URL = obj.Spec.Git.URL
+		dbObj.Certificate = obj.Spec.Git.Certificate.String()
+		dbObj.Username = obj.Spec.Git.Username.String()
+		dbObj.Password = obj.Spec.Git.Password.String()
+		dbObj.Properties = map[string]string{
+			"ref": obj.Spec.Git.Ref,
+		}
+	}
+
+	if obj.Spec.GitHub != nil {
+		dbObj.Type = models.ConnectionTypeGithub
+		dbObj.Password = obj.Spec.GitHub.PersonalAccessToken.String()
+	}
+
+	if obj.Spec.HTTP != nil {
+		dbObj.Type = models.ConnectionTypeHTTP
+		dbObj.URL = obj.Spec.HTTP.URL
+		dbObj.Username = obj.Spec.HTTP.Username.String()
+		dbObj.Password = obj.Spec.HTTP.Password.String()
+		dbObj.Properties = map[string]string{
+			"insecure_tls": strconv.FormatBool(obj.Spec.HTTP.InsecureTLS),
+		}
+	}
+
+	if obj.Spec.Kubernetes != nil {
+		dbObj.Type = models.ConnectionTypeKubernetes
+		dbObj.Certificate = obj.Spec.Kubernetes.Certificate.String()
+	}
+
+	if obj.Spec.Mongo != nil {
+		dbObj.Type = models.ConnectionTypeMongo
+
+		if obj.Spec.Mongo.URL.String() != "" {
+			dbObj.URL = obj.Spec.Mongo.URL.String()
+		} else {
+			dbObj.URL = "mongodb://$(username):$(password)@$(properties.host)/?$(properties.database)"
+			queryParams := url.Values{}
+			if obj.Spec.Mongo.ReplicaSet != "" {
+				queryParams.Set("replicaSet", obj.Spec.Mongo.ReplicaSet)
+			}
+			if obj.Spec.Mongo.InsecureTLS {
+				queryParams.Set("tls", "true")
+			}
+			if len(queryParams) > 0 {
+				dbObj.URL += queryParams.Encode()
+			}
+
+			dbObj.Username = obj.Spec.Mongo.Username.String()
+			dbObj.Password = obj.Spec.Mongo.Password.String()
+			dbObj.Properties = map[string]string{
+				"host":         obj.Spec.Mongo.Host.String(),
+				"database":     obj.Spec.Mongo.Database.String(),
+				"replica_set":  obj.Spec.Mongo.ReplicaSet,
+				"insecure_tls": strconv.FormatBool(obj.Spec.Mongo.InsecureTLS),
+			}
+		}
+	}
+
+	if obj.Spec.MSSQL != nil {
+		dbObj.Type = models.ConnectionTypeSQLServer
+
+		if obj.Spec.MSSQL.URL.String() != "" {
+			dbObj.URL = obj.Spec.MSSQL.URL.String()
+		} else {
+			dbObj.URL = `Server=$(properties.host);Database=$(properties.database);User Id=$(username);Password=$(password);TrustServerCertificate=$(properties.trust_server_certificate)`
+			dbObj.Username = obj.Spec.MSSQL.Username.String()
+			dbObj.Password = obj.Spec.MSSQL.Password.String()
+			dbObj.Properties = map[string]string{
+				"host":                     obj.Spec.MSSQL.Host.String(),
+				"database":                 obj.Spec.MSSQL.Database.String(),
+				"trust_server_certificate": strconv.FormatBool(obj.Spec.MSSQL.TrustServerCertificate),
+			}
+		}
+	}
+
+	if obj.Spec.MySQL != nil {
+		dbObj.Type = models.ConnectionTypeMySQL
+		if obj.Spec.MySQL.URL.String() != "" {
+			dbObj.URL = obj.Spec.MySQL.URL.String()
+		} else {
+			dbObj.URL = "mysql://$(username):$(password)@$(properties.host)/$(properties.database)"
+			if obj.Spec.MySQL.InsecureTLS {
+				dbObj.URL += "sslMode=disabled"
+			}
+
+			dbObj.Username = obj.Spec.MySQL.Username.String()
+			dbObj.Password = obj.Spec.MySQL.Password.String()
+			dbObj.Properties = map[string]string{
+				"host":         obj.Spec.MySQL.Host.String(),
+				"database":     obj.Spec.MySQL.Database.String(),
+				"insecure_tls": strconv.FormatBool(obj.Spec.MySQL.InsecureTLS),
+			}
+		}
+	}
+
+	if obj.Spec.Postgres != nil {
+		dbObj.Type = models.ConnectionTypePostgres
+		if obj.Spec.Postgres.URL.String() != "" {
+			dbObj.URL = obj.Spec.Postgres.URL.String()
+		} else {
+			dbObj.URL = "postgres://$(username):$(password)@$(properties.host)/$(properties.database)"
+			if obj.Spec.Postgres.InsecureTLS {
+				dbObj.URL += "?sslmode=disable"
+			}
+
+			dbObj.Username = obj.Spec.Postgres.Username.String()
+			dbObj.Password = obj.Spec.Postgres.Password.String()
+			dbObj.Properties = map[string]string{
+				"host":         obj.Spec.Postgres.Host.String(),
+				"database":     obj.Spec.Postgres.Database.String(),
+				"insecure_tls": strconv.FormatBool(obj.Spec.Postgres.InsecureTLS),
+			}
+		}
+	}
+
+	if obj.Spec.SFTP != nil {
+		dbObj.Type = models.ConnectionTypeSFTP
+		dbObj.URL = obj.Spec.SFTP.Host.String()
+		dbObj.Username = obj.Spec.SFTP.Username.String()
+		dbObj.Password = obj.Spec.SFTP.Password.String()
+		dbObj.Properties = map[string]string{
+			"path": obj.Spec.SFTP.Path,
+			"port": strconv.Itoa(obj.Spec.SFTP.Port),
+		}
+	}
+
+	if obj.Spec.SMB != nil {
+		dbObj.Type = models.ConnectionTypeSMB
+		dbObj.URL = obj.Spec.SMB.Server.String()
+		dbObj.Username = obj.Spec.SMB.Username.String()
+		dbObj.Password = obj.Spec.SMB.Password.String()
+		dbObj.Properties = map[string]string{
+			"port": obj.Spec.SMB.Port.String(),
+		}
+	}
+
+	if obj.Spec.Discord != nil {
+		dbObj.URL = "discord://$(password)@$(username)"
+		dbObj.Type = models.ConnectionTypeDiscord
+		dbObj.Username = obj.Spec.Discord.WebhookID
+		dbObj.Password = obj.Spec.Discord.Token
+	}
+
+	if obj.Spec.SMTP != nil {
+		dbObj.URL = fmt.Sprintf("smtp://$(username):$(password)@$%s:%d/?UseStartTLS=%s&Encryption=%s&Auth=%s",
+			obj.Spec.SMTP.Host,
+			obj.Spec.SMTP.Port,
+			strconv.FormatBool(obj.Spec.SMTP.InsecureTLS),
+			obj.Spec.SMTP.Encryption,
+			obj.Spec.SMTP.Auth,
+		)
+		dbObj.Type = models.ConnectionTypeEmail
+		dbObj.URL = obj.Spec.SMTP.Host
+		dbObj.Username = obj.Spec.SMTP.Username.String()
+		dbObj.Password = obj.Spec.SMTP.Password.String()
+		dbObj.Properties = map[string]string{
+			"port":        strconv.Itoa(obj.Spec.SMTP.Port),
+			"subject":     obj.Spec.SMTP.Subject,
+			"auth":        obj.Spec.SMTP.Auth,
+			"fromAddress": obj.Spec.SMTP.FromAddress,
+			"toAddress":   strings.Join(obj.Spec.SMTP.ToAddresses, ", "),
+		}
+	}
+
+	if obj.Spec.Ntfy != nil {
+		dbObj.URL = fmt.Sprintf("ntfy://$(username):$(password)@%s/%s", obj.Spec.Ntfy.Host, obj.Spec.Ntfy.Topic)
+		dbObj.Type = models.ConnectionTypeNtfy
+		dbObj.Username = obj.Spec.Ntfy.Username.String()
+		dbObj.Password = obj.Spec.Ntfy.Password.String()
+		dbObj.Properties = map[string]string{
+			"topic": obj.Spec.Ntfy.Topic,
+		}
+	}
+
+	if obj.Spec.Pushbullet != nil {
+		targets := strings.Join(obj.Spec.Pushbullet.Targets, ",")
+
+		dbObj.URL = fmt.Sprintf("pushbullet://$(password)/%s", targets)
+		dbObj.Type = models.ConnectionTypePushbullet
+		dbObj.Password = obj.Spec.Pushbullet.Token.String()
+		dbObj.Properties = map[string]string{
+			"targets": targets,
+		}
+	}
+
+	if obj.Spec.Pushover != nil {
+		dbObj.URL = "pushover://:$(password)@$(username)"
+		dbObj.Type = models.ConnectionTypePushover
+		dbObj.Username = obj.Spec.Pushover.User
+		dbObj.Password = obj.Spec.Pushover.Token.String()
+	}
+
+	if obj.Spec.Slack != nil {
+		dbObj.URL = "slack://$(password)@$(username)"
+		dbObj.Type = models.ConnectionTypeSlack
+		dbObj.Username = obj.Spec.Slack.Channel
+		dbObj.Password = obj.Spec.Slack.Token.String()
+		dbObj.Properties = map[string]string{
+			"BotName": obj.Spec.Slack.BotName,
+		}
+	}
+
+	if obj.Spec.Telegram != nil {
+		dbObj.URL = "telegram://$(password)@telegram/?Chats=$(username)"
+		dbObj.Type = models.ConnectionTypeTelegram
+		dbObj.Username = obj.Spec.Telegram.Chats.String()
+		dbObj.Password = obj.Spec.Telegram.Token.String()
 	}
 
 	return ctx.DB().Save(&dbObj).Error
