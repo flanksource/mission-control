16a41d5c2c93055c39613560d9ef2110b585ea91
Aditya Thebe <contact@adityathebe.com>
2024-05-03 19:30:09 +0545

    feat: cleanup stale running job histories

--- jobs/job_history_cleanup.go
+++ jobs/job_history_cleanup.go
@@ -1,11 +1,34 @@
 package jobs
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/flanksource/duty/job"
+	"github.com/flanksource/duty/models"
 )
 
-func CleanupJobHistoryTable(ctx job.JobRuntime) error {
-	res := ctx.DB().Exec("DELETE FROM job_history where now() - created_at > interval '30 day'")
-	ctx.History.SuccessCount = int(res.RowsAffected)
-	return res.Error
+var cleanupStaleJobHistory = &job.Job{
+	Name:       "CleanupStaleJobHistory",
+	Schedule:   "@every 4h",
+	Singleton:  true,
+	JobHistory: true,
+	Retention:  job.RetentionFew,
+	RunNow:     true,
+	Fn: func(ctx job.JobRuntime) error {
+		staleHistoryMaxAge := ctx.Properties().Duration("job.history.maxAge", time.Hour*24*30)
+		count, err := job.CleanupStaleHistory(ctx.Context, staleHistoryMaxAge, "", "")
+		if err != nil {
+			return fmt.Errorf("error cleaning stale job histories: %w", err)
+		}
+
+		staleRunningHistoryMaxAge := ctx.Properties().Duration("job.history.running.maxAge", time.Hour*4)
+		runningStale, err := job.CleanupStaleHistory(ctx.Context, staleRunningHistoryMaxAge, "", "", models.StatusRunning)
+		if err != nil {
+			return fmt.Errorf("error cleaning stale RUNNING job histories: %w", err)
+		}
+
+		ctx.History.SuccessCount = count + runningStale
+		return nil
+	},
 }
