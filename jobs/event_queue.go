fa267a5f6d3ba79caaa8f69e1914c077abb7c333
Aditya Thebe <contact@adityathebe.com>
2024-05-09 12:20:46 +0545

    fix: success reporting in event queue cleanup job (#1003)
    * fix: success reporting in event queue cleanup job
    
    * fix: remove cleanup related to `push_queue.create`
    
    * chore: get event_queue max age from properties

--- jobs/event_queue.go
+++ jobs/event_queue.go
@@ -3,44 +3,21 @@ package jobs
 import (
 	"time"
 
-	"github.com/flanksource/commons/logger"
 	"github.com/flanksource/duty/job"
 )
 
+const eventQueueStaleAge = time.Hour * 24 * 30
+
 // CleanupEventQueue deletes stale records in the `event_queue` table
 func CleanupEventQueue(ctx job.JobRuntime) error {
-
-	pushQueueSchedule := map[string]time.Duration{
-		"topologies":                     time.Hour * 24 * 7,
-		"canaries":                       time.Hour * 24 * 7,
-		"config_scrapers":                time.Hour * 24 * 7,
-		"checks":                         time.Hour * 24 * 7,
-		"components":                     time.Hour * 24 * 7,
-		"config_items":                   time.Hour * 24 * 7,
-		"config_analysis":                time.Hour * 24 * 7,
-		"config_changes":                 time.Hour * 24 * 7,
-		"config_component_relationships": time.Hour * 24 * 7,
-		"component_relationships":        time.Hour * 24 * 7,
-		"config_relationships":           time.Hour * 24 * 7,
-		"check_statuses":                 time.Hour * 24 * 7,
-	}
-
-	for table, age := range pushQueueSchedule {
-		result := ctx.DB().Exec("DELETE FROM event_queue WHERE name = 'push_queue.create' AND properties->>'table' = ? AND NOW() - created_at > ?", table, age)
-		if result.Error != nil {
-			ctx.History.AddError(result.Error.Error())
-		} else if result.RowsAffected > 0 {
-			logger.Warnf("Deleted %d push_queue events for table=%s", result.RowsAffected, table)
-			ctx.History.ErrorCount += int(result.RowsAffected)
-		}
-	}
-
-	defaultAge := time.Hour * 24 * 30
-	result := ctx.DB().Exec("DELETE FROM event_queue WHERE name != 'push_queue.create' AND NOW() - created_at > ?", defaultAge)
+	age := ctx.Properties().Duration("event_queue.maxAge", eventQueueStaleAge)
+	result := ctx.DB().Exec("DELETE FROM event_queue WHERE NOW() - created_at > ?", age)
 	if result.Error != nil {
-		ctx.History.AddError(result.Error.Error())
-	} else if result.RowsAffected > 0 {
-		ctx.History.ErrorCount += int(result.RowsAffected)
+		return result.Error
+	}
+
+	if result.RowsAffected > 0 {
+		ctx.History.SuccessCount += int(result.RowsAffected)
 	}
 
 	return nil
