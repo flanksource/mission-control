16d742d76ae368d78b1bd7a8161eadc674ceb85f
Aditya Thebe <contact@adityathebe.com>
2024-05-22 14:35:33 +0545

    feat: add counter metrics for rate limited notifications

--- notification/events.go
+++ notification/events.go
@@ -127,6 +127,7 @@ func addNotificationEvent(ctx context.Context, event postq.Event) error {
 			if !rateLimiter.Allow() {
 				// rate limited notifications are simply dropped.
 				logger.Warnf("notification(%s) rate limited for event=%s", id, event.Name)
+				rateLimitedCounter.WithLabelValues(id).Inc()
 				continue
 			}
 
