a7f172af375eb380e0506bd5823482109a35b66f
Moshe Immermam <moshe@flanksource.com>
2024-08-16 00:53:56 +0300

    chore: fix interpreter detection

--- playbook/actions/interpreter.go
+++ playbook/actions/interpreter.go
@@ -7,75 +7,78 @@ import (
 	"strings"
 
 	"github.com/flanksource/duty/context"
-	"github.com/samber/lo"
-	"github.com/samber/oops"
 )
 
 var DefaultInterpreter string
+var DefaultInterpreterArgs []string
 
 func init() {
-	DefaultInterpreter = DetectDefaultInterpreter()
+	DefaultInterpreter, DefaultInterpreterArgs = DetectDefaultInterpreter()
 }
 
-// ParseShebangLine reads the first line of the script to detect the interpreter from the shebang line.
-func ParseShebangLine(script string) (string, []string) {
+// CreateCommandFromScript creates an os/exec.Cmd from the script, using the interpreter specified in the shebang line if present.
+func CreateCommandFromScript(ctx context.Context, script string) (*exec.Cmd, error) {
+	interpreter, args := DetectInterpreterFromShebang(script)
+	script = TrimLine(script, "#!")
+	if script == "" {
+		return nil, ctx.Oops().Errorf("empty script")
+	}
+	args = append(args, script)
+	return exec.CommandContext(ctx, interpreter, args...), nil
+}
+
+func TrimLine(lines string, prefix string) string {
+	s := []string{}
+	for _, line := range strings.Split(lines, "\n") {
+		if !strings.HasPrefix(line, prefix) {
+			s = append(s, line)
+		}
+	}
+	return strings.Join(s, "\n")
+}
+
+// DetectInterpreterFromShebang reads the first line of the script to detect the interpreter from the shebang line.
+func DetectInterpreterFromShebang(script string) (string, []string) {
 	reader := strings.NewReader(script)
 	scanner := bufio.NewScanner(reader)
 	if scanner.Scan() {
 		firstLine := scanner.Text()
 		if strings.HasPrefix(firstLine, "#!") {
 			parts := strings.Fields(strings.TrimSpace(firstLine[2:]))
-			if len(parts) > 0 {
-				return parts[0], parts[1:]
+			if len(parts) == 0 {
+				return "", nil
+			} else if len(parts) == 1 {
+				// No args, just interpreter and assume it supports the -c flag
+				return parts[0], []string{"-c"}
 			}
+			return parts[0], parts[1:]
 		}
 	}
-	return "", nil
-}
-
-// CreateCommandFromScript creates an os/exec.Cmd from the script, using the interpreter specified in the shebang line if present.
-func CreateCommandFromScript(ctx context.Context, script string) (*exec.Cmd, error) {
-	interpreter, args := ParseShebangLine(script)
-	args = append([]string{"-c", script}, args...)
-	return exec.CommandContext(ctx, lo.CoalesceOrEmpty(interpreter, DefaultInterpreter), args...), nil
-}
-
-// DetectInterpreterFromShebang reads the first line of the script to detect the interpreter from the shebang line.
-func DetectInterpreterFromShebang(script string) (string, error) {
-	reader := strings.NewReader(script)
-	scanner := bufio.NewScanner(reader)
-	if scanner.Scan() {
-		firstLine := scanner.Text()
-		if strings.HasPrefix(firstLine, "#!") {
-			return strings.TrimSpace(firstLine[2:]), nil
-		}
-	}
-	return "", oops.Errorf("no shebang line found")
+	return DefaultInterpreter, DefaultInterpreterArgs
 }
 
 // DetectDefaultInterpreter detects the default interpreter based on the OS.
-func DetectDefaultInterpreter() string {
+func DetectDefaultInterpreter() (string, []string) {
 	switch runtime.GOOS {
 	case "windows":
 		// Check for PowerShell on Windows
 		if _, err := exec.LookPath("pwsh.exe"); err == nil {
-			return "pwsh.exe"
+			return "pwsh.exe", []string{"-c"}
 		}
 		// Fallback to cmd if PowerShell is not found
 		if _, err := exec.LookPath("cmd.exe"); err == nil {
-			return "cmd.exe"
+			return "cmd.exe", []string{"-c"}
 		}
-		return ""
 
 	default:
 		// Check for Bash on Unix-like systems
 		if _, err := exec.LookPath("bash"); err == nil {
-			return "bash"
+			return "bash", []string{"-c"}
 		}
 		// Fallback to sh if Bash is not found
 		if _, err := exec.LookPath("sh"); err == nil {
-			return "sh"
+			return "sh", []string{"-c"}
 		}
-		return ""
 	}
+	return "", nil
 }
