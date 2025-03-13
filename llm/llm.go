ae55b65e41111f42425d917968f4255e87d8edab
Aditya Thebe <contact@adityathebe.com>
2025-03-13 20:40:22 +0545

    feat: structured output for Anthropic

--- llm/llm.go
+++ llm/llm.go
@@ -28,12 +28,13 @@ type Config struct {
 	ResponseFormat ResponseFormat
 }
 
-func Prompt(ctx context.Context, config Config, systemPrompt string, promptParts ...string) (string, []llms.MessageContent, error) {
-	model, err := getLLMModel(ctx, config)
-	if err != nil {
-		return "", nil, err
-	}
+const (
+	forceToolUsePrompt = `You MUST use the %s tool to extract the diagnosis information. 
+	Do not provide any other response format. 
+	Only use the tool to respond.`
+)
 
+func Prompt(ctx context.Context, config Config, systemPrompt string, promptParts ...string) (string, []llms.MessageContent, error) {
 	content := []llms.MessageContent{
 		llms.TextParts(llms.ChatMessageTypeSystem, systemPrompt),
 	}
@@ -42,17 +43,7 @@ func Prompt(ctx context.Context, config Config, systemPrompt string, promptParts
 		content = append(content, llms.TextParts(llms.ChatMessageTypeHuman, p))
 	}
 
-	resp, err := model.GenerateContent(ctx, content, llms.WithTemperature(0))
-	if err != nil {
-		return "", nil, fmt.Errorf("failed to generate response: %w", err)
-	}
-
-	if len(resp.Choices) == 0 {
-		return "", nil, errors.New("no response from LLM")
-	}
-
-	content = append(content, llms.TextParts(llms.ChatMessageTypeAI, resp.Choices[0].Content))
-	return resp.Choices[0].Content, content, nil
+	return PromptWithHistory(ctx, config, content, "")
 }
 
 func PromptWithHistory(ctx context.Context, config Config, history []llms.MessageContent, prompt string) (string, []llms.MessageContent, error) {
@@ -61,9 +52,46 @@ func PromptWithHistory(ctx context.Context, config Config, history []llms.Messag
 		return "", nil, err
 	}
 
-	content := append(history, llms.TextParts(llms.ChatMessageTypeHuman, prompt))
+	messages := append(history, llms.TextParts(llms.ChatMessageTypeHuman, prompt))
 
-	resp, err := model.GenerateContent(ctx, content, llms.WithTemperature(0))
+	options := []llms.CallOption{llms.WithTemperature(0)}
+
+	// Add backend-specific options for tool choice
+	switch config.Backend {
+	case api.LLMBackendOpenAI:
+		// Do nothing
+		// NOTE: we use `response_format` instead of function calling.
+		// OpenAI does support function calling, but I don't think we can force the model to use that tool
+		// like we can with Anthropic.
+
+	default:
+		if config.ResponseFormat == ResponseFormatDiagnosis {
+			options = append(options, llms.WithTools([]llms.Tool{diagnosisTool}))
+			messages = append(messages, llms.TextParts(llms.ChatMessageTypeHuman, fmt.Sprintf(forceToolUsePrompt, diagnosisTool.Function.Name)))
+		} else if config.ResponseFormat == ResponseFormatPlaybookRecommendations {
+			options = append(options, llms.WithTools([]llms.Tool{playbookRecommendationsTool}))
+			messages = append(messages, llms.TextParts(llms.ChatMessageTypeHuman, fmt.Sprintf(forceToolUsePrompt, playbookRecommendationsTool.Function.Name)))
+		}
+
+		// NOTE: Anthropic & Gemini have forced tool use, but it's not supported in LangChainGo.
+		// So, we force that with prompts for now.
+		//
+		// https://docs.anthropic.com/en/docs/build-with-claude/tool-use/overview#forcing-tool-use
+		// options = append(options, llms.WithToolChoice(map[string]any{
+		// 	"type": "tool",
+		// 	"name": "extract_diagnosis",
+		// }))
+		//
+		// Use the correct format for Gemini function calling
+		// options = append(options, llms.WithToolChoice(map[string]any{
+		// 	"function_calling_config": map[string]any{
+		// 		"mode":                   "ANY",
+		// 		"allowed_function_names": []string{"extract_diagnosis"},
+		// 	},
+		// }))
+	}
+
+	resp, err := model.GenerateContent(ctx, messages, options...)
 	if err != nil {
 		return "", nil, fmt.Errorf("failed to generate response: %w", err)
 	}
@@ -72,8 +100,18 @@ func PromptWithHistory(ctx context.Context, config Config, history []llms.Messag
 		return "", nil, errors.New("no response from LLM")
 	}
 
-	content = append(content, llms.TextParts(llms.ChatMessageTypeAI, resp.Choices[0].Content))
-	return resp.Choices[0].Content, content, nil
+	aiResponse := resp.Choices[0].Content
+
+	// We prioritize response from tools if available
+	for _, choice := range resp.Choices {
+		if len(choice.ToolCalls) > 0 {
+			aiResponse = choice.ToolCalls[0].FunctionCall.Arguments
+			break
+		}
+	}
+
+	messages = append(messages, llms.TextParts(llms.ChatMessageTypeAI, aiResponse))
+	return aiResponse, messages, nil
 }
 
 func getLLMModel(ctx context.Context, config Config) (llms.Model, error) {
@@ -95,24 +133,7 @@ func getLLMModel(ctx context.Context, config Config) (llms.Model, error) {
 				JSONSchema: &openai.ResponseFormatJSONSchema{
 					Name:   "diagnosis",
 					Strict: true,
-					Schema: &openai.ResponseFormatJSONSchemaProperty{
-						Type: "object",
-						Properties: map[string]*openai.ResponseFormatJSONSchemaProperty{
-							"headline": {
-								Type:        "string",
-								Description: "Headline that clearly mentions the affected resource & the issue. Feel free to add emojis. Keep it short and concise.",
-							},
-							"summary": {
-								Type:        "string",
-								Description: "Summary of the issue in markdown. Use bullet points if needed.",
-							},
-							"recommended_fix": {
-								Type:        "string",
-								Description: "Short and concise recommended fix for the issue in markdown. Use bullet points if needed.",
-							},
-						},
-						Required: []string{"headline", "summary", "recommended_fix"},
-					},
+					Schema: &diagnosisToolSchema,
 				},
 			})
 			opts = append(opts, openaiResponseFormatOpt)
@@ -121,42 +142,8 @@ func getLLMModel(ctx context.Context, config Config) (llms.Model, error) {
 				Type: "json_schema",
 				JSONSchema: &openai.ResponseFormatJSONSchema{
 					Name: "playbook_recommendations",
-					Schema: &openai.ResponseFormatJSONSchemaProperty{
-						Type: "object",
-						Properties: map[string]*openai.ResponseFormatJSONSchemaProperty{
-							"playbooks": {
-								Type:        "array",
-								Description: "List of recommended playbooks to fix the issue. The playbooks are sorted by relevance to the issue. Only include playbooks that are relevant to the issue. It's okay if the list is empty.",
-								Items: &openai.ResponseFormatJSONSchemaProperty{
-									Type: "object",
-									Properties: map[string]*openai.ResponseFormatJSONSchemaProperty{
-										"id": {
-											Type:        "string",
-											Description: "The UUID of the playbook",
-										},
-										"emoji": {
-											Type:        "string",
-											Description: "The emoji to represent the playbook",
-										},
-										"title": {
-											Type:        "string",
-											Description: "The title of the playbook",
-										},
-										"parameters": {
-											Type:                 "object",
-											Description:          "A key-value (Record<string, string>) pair of parameters to pass to the playbook. Keep in mind the values are all strings even numbers and booleans are strings",
-											AdditionalProperties: true,
-										},
-										"resource_id": {
-											Type: "string",
-										},
-									},
-									Required: []string{"id", "emoji", "name", "parameters", "resource_id"},
-								},
-							},
-						},
-						Required: []string{"playbooks"},
-					},
+					// Strict: true, // NOTE: cannot set this to strict, because playbook.parameters must be additionalProperties=true
+					Schema: &recommendatationToolSchema,
 				},
 			})
 			opts = append(opts, openaiResponseFormatOpt)
