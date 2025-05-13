35053a89c75f166daaa99af6b7a14ec3d19149a8
Aditya Thebe <contact@adityathebe.com>
2025-05-13 20:45:30 +0545

    feat: save applications to DB

---
+++ db/applications.go
@@ -0,0 +1,45 @@
+package db
+
+import (
+	"encoding/json"
+
+	"github.com/flanksource/duty"
+	"github.com/flanksource/duty/context"
+	"github.com/flanksource/duty/models"
+	v1 "github.com/flanksource/incident-commander/api/v1"
+	"github.com/google/uuid"
+)
+
+func PersistApplicationFromCRD(ctx context.Context, obj *v1.Application) error {
+	uid, err := uuid.Parse(string(obj.GetUID()))
+	if err != nil {
+		return err
+	}
+
+	spec, err := json.Marshal(obj.Spec)
+	if err != nil {
+		return err
+	}
+
+	app := models.Application{
+		ID:          uid,
+		Name:        obj.Name,
+		Namespace:   obj.Namespace,
+		Description: obj.Spec.Description,
+		Spec:        string(spec),
+		Source:      models.SourceCRD,
+	}
+
+	return ctx.DB().Save(&app).Error
+}
+
+func DeleteApplication(ctx context.Context, id string) error {
+	return ctx.DB().Model(&models.Application{}).Where("id = ?", id).Update("deleted_at", duty.Now()).Error
+}
+
+func DeleteStaleApplication(ctx context.Context, newer *v1.Application) error {
+	return ctx.DB().Model(&models.Application{}).
+		Where("name = ? AND namespace = ?", newer.Name, newer.Namespace).
+		Where("deleted_at IS NULL").
+		Update("deleted_at", duty.Now()).Error
+}
