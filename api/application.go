14112756c74a2e89c2ff2af9559f6476546be7a7
Aditya Thebe <contact@adityathebe.com>
2025-06-12 18:06:55 +0545

    feat: application changes

--- api/application.go
+++ api/application.go
@@ -77,8 +77,6 @@ type ApplicationChange struct {
 	User        string    `json:"user"`
 	Description string    `json:"description"`
 	Status      string    `json:"status"`
-	CreatedAt   time.Time `json:"createdAt"`
-	UpdatedAt   time.Time `json:"updatedAt"`
 }
 
 type ApplicationAccessControl struct {
