d8aef9bde7574f69c2e400893b98045a579559e2
Aditya Thebe <contact@adityathebe.com>
2025-12-03 15:05:37 +0545

    test: View SQL data query

--- views/suite_test.go
+++ views/suite_test.go
@@ -1,9 +1,11 @@
 package views
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/flanksource/duty/context"
+	"github.com/flanksource/duty/models"
 	"github.com/flanksource/duty/tests/setup"
 	ginkgo "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -16,10 +18,26 @@ func TestViews(t *testing.T) {
 
 var (
 	DefaultContext context.Context
+	conn           models.Connection
+	connectionName string
 )
 
 var _ = ginkgo.BeforeSuite(func() {
 	DefaultContext = setup.BeforeSuiteFn()
+
+	conn = models.Connection{
+		Name:      "test-db-connection",
+		Namespace: "default",
+		Type:      models.ConnectionTypePostgres,
+		URL:       DefaultContext.Value("db_url").(string),
+		Source:    models.SourceUI,
+	}
+	Expect(DefaultContext.DB().Save(&conn).Error).ToNot(HaveOccurred())
+
+	connectionName = fmt.Sprintf("connection://%s/%s", conn.Namespace, conn.Name)
 })
 
-var _ = ginkgo.AfterSuite(setup.AfterSuiteFn)
+var _ = ginkgo.AfterSuite(func() {
+	Expect(DefaultContext.DB().Delete(&conn).Error).ToNot(HaveOccurred())
+	setup.AfterSuiteFn()
+})
