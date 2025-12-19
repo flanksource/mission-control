ef4032b04c6be1a94139d4eb060561316b90b047
Aditya Thebe <contact@adityathebe.com>
2025-12-19 17:21:49 +0545

    feat: Added GET /connection/llm endpoint that returns hydrated connection of the default LLM provider

--- db/connections.go
+++ db/connections.go
@@ -9,12 +9,14 @@ import (
 
 	"github.com/flanksource/commons/collections"
 	"github.com/flanksource/duty"
+	"github.com/flanksource/duty/api"
 	"github.com/flanksource/duty/context"
 	"github.com/flanksource/duty/models"
-	v1 "github.com/flanksource/incident-commander/api/v1"
-	"github.com/flanksource/incident-commander/utils"
 	"github.com/google/uuid"
 	"github.com/samber/lo"
+
+	v1 "github.com/flanksource/incident-commander/api/v1"
+	"github.com/flanksource/incident-commander/utils"
 )
 
 func PersistConnectionFromCRD(ctx context.Context, obj *v1.Connection) error {
@@ -27,7 +29,6 @@ func PersistConnectionFromCRD(ctx context.Context, obj *v1.Connection) error {
 		Username:    obj.Spec.Username.String(),
 		Password:    obj.Spec.Password.String(),
 		Certificate: obj.Spec.Certificate.String(),
-		Properties:  obj.Spec.Properties,
 		InsecureTLS: obj.Spec.InsecureTLS,
 		Source:      models.SourceCRD,
 		// Gorm.Save does not use defaults when inserting
@@ -432,6 +433,11 @@ func PersistConnectionFromCRD(ctx context.Context, obj *v1.Connection) error {
 	}
 
 	obj.Status.Ref = fmt.Sprintf("connection://%s/%s", obj.Namespace, obj.Name)
+
+	if len(obj.Spec.Properties) != 0 {
+		dbObj.Properties = collections.MergeMap(obj.Spec.Properties, dbObj.Properties)
+	}
+
 	return ctx.DB().Save(&dbObj).Error
 }
 
@@ -450,4 +456,17 @@ func ListConnections(ctx context.Context) ([]models.Connection, error) {
 	var c []models.Connection
 	err := ctx.DB().Omit("password", "certificate").Where("deleted_at IS NULL").Find(&c).Error
 	return c, err
+}
+
+func FindDefaultLLMProviderConnection(ctx context.Context) (*models.Connection, error) {
+	var c models.Connection
+	if err := ctx.DB().Where("properties->>'defaultLLMProvider' = 'true'").Find(&c).Error; err != nil {
+		return nil, err
+	}
+
+	if c.ID == uuid.Nil {
+		return nil, api.Errorf(api.ENOTFOUND, "a default connection was not found")
+	}
+
+	return &c, nil
 }
