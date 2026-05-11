00c5145b05e0f82cbe1c8ab3dde982528767f42d
Moshe Immerman <moshe@flanksource.com>
2026-05-11 18:35:31 +0545

    feat: new controllers

--- api/playbook.go
+++ api/playbook.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"time"
+
 	"github.com/flanksource/duty/types"
 	"github.com/google/uuid"
 )
@@ -8,7 +10,15 @@ import (
 // PlaybookListItem is the response struct for listing playbooks
 // for a filter/selector.
 type PlaybookListItem struct {
-	ID         uuid.UUID  `json:"id"`
-	Name       string     `json:"name"`
-	Parameters types.JSON `json:"parameters,omitempty"`
+	ID          uuid.UUID  `json:"id"`
+	Namespace   string     `json:"namespace,omitempty"`
+	Name        string     `json:"name"`
+	Title       string     `json:"title,omitempty"`
+	Icon        string     `json:"icon,omitempty"`
+	Description string     `json:"description,omitempty"`
+	Source      string     `json:"source,omitempty"`
+	Category    string     `json:"category,omitempty"`
+	CreatedAt   *time.Time `json:"created_at,omitempty"`
+	Parameters  types.JSON `json:"parameters,omitempty"`
+	Spec        types.JSON `json:"spec,omitempty"`
 }
