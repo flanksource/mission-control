8211b0b300eb0e5cf55cbfd8747df6b416f0f227
Aditya Thebe <contact@adityathebe.com>
2026-05-11 20:32:13 +0545

    wire up supervisor

--- cmd/server.go
+++ cmd/server.go
@@ -33,6 +33,7 @@ import (
 	"github.com/flanksource/incident-commander/metrics"
 	"github.com/flanksource/incident-commander/notification"
 	pluginregistry "github.com/flanksource/incident-commander/plugin/registry"
+	"github.com/flanksource/incident-commander/plugin/supervisor"
 	echov4 "github.com/labstack/echo/v4"
 
 	// register event handlers & echo routers
@@ -157,6 +158,7 @@ func launchKopper(ctx context.Context) {
 		shutdown.ShutdownAndExit(1, fmt.Sprintf("Unable to create controller for Team: %v", err))
 	}
 
+	supervisor.Wire(ctx)
 	if _, err := kopper.SetupReconciler(ctx, mgr,
 		pluginregistry.PersistPluginFromCRD,
 		pluginregistry.DeletePlugin,
